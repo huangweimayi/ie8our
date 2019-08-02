49c11928798be75de6455151ae49742a2c02cb96
huangwei <793024079@qq.com>
2019-08-02 09:21:18 +0800

    1

--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,6 @@
 var conf = {
-  serverHost: 'http://demo.jixinghai.com/xiaobaidaojia/public/index.php',
-  // serverHost: 'http://devdaojiam.qingmh.com',
+  // serverHost: 'http://demo.jixinghai.com/xiaobaidaojia/public/index.php',
+  serverHost: 'http://devdaojiam.qingmh.com',
   // serverHost: 'http://o2o.114yun.net/sysAdmin',
   // serverHost: 'http://o2o.114yun.net/sysAdmindev',
   // serverHost: 'http://devdaojiam.qingmh.com',
